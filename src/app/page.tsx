70473b18851995ad61a400e7a40e19be06b2eaaf
André Rocha <andre.rocha@smarttravelit.com.br>
2024-11-03 01:42:25 -0300

    DataBase setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/JhWsSz31MqySi4zXnpbadTOHbU5c0GJCyv73kfFonLpASjlK",
@@ -20,16 +21,23 @@ const mockImages = mockUrls.map((url, index) => ({
 
 
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">{
-        mockImages.map((image) => (
-          <div key={image.id} className="w-48">
+      <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48" >
             <img src={image.url} />
           </div>
-        ))
-      }
+        ))}
       </div>
     </main>
   );
