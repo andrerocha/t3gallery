2c6ebb36658421b1cb2a484fe9fee73ac8464c35
André Rocha Araujo <andre_rocha@msn.com>
2024-11-04 00:21:02 -0300

    linkg database to ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 import { db } from "~/server/db";
 
-export const dynamic = "forece-dynamic";
+export const dynamic = "force-dynamic";
 
 const mockUrls = [
   "https://utfs.io/f/JhWsSz31MqySi4zXnpbadTOHbU5c0GJCyv73kfFonLpASjlK",
@@ -9,8 +9,7 @@ const mockUrls = [
   "https://utfs.io/f/JhWsSz31MqySXdrn5qDtjmr9VKHpMi261YeZTDwzgqUSFaA5",
   "https://utfs.io/f/JhWsSz31MqyStYqTckZAR3tzX8dh2ZeDulKHwkgcnsVqM75p",
   "https://utfs.io/f/JhWsSz31MqyS3dlBmEaMhxuWw8gRAt3TnUHJq1cIfaPyXFZi",
-  "https://utfs.io/f/JhWsSz31MqySszrgpDWLdANhbUQsGC7YfTIt1c9e4nPjJEVy",
-  "https://utfs.io/f/JhWsSz31MqySszrgpDWLdANhbUQsGC7YfTIt1c9e4nPjJEVy",
+  "https://utfs.io/f/JhWsSz31MqySszrgpDWLdANhbUQsGC7YfTIt1c9e4nPjJEVy",  
   "https://utfs.io/f/JhWsSz31MqySaooutzUFTGpHLSYfNPXBwgt752OM4aVlEhcD",
   "https://utfs.io/f/JhWsSz31MqySNawdYofZzAHacs4nuwhtp1BRqrDF895ImLY2",
   "https://utfs.io/f/JhWsSz31MqySZ6NWCD4NjRXr7FU1yBMKEADzuP89JdGxkgSV"
@@ -21,23 +20,19 @@ const mockImages = mockUrls.map((url, index) => ({
   url
 }));
 
-
-
 export default async function HomePage() {
 
-  const posts = await db.query.posts.findMany();
-
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id)
+  });  
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48" >
+        {[...images, ...images, ...images].map((image, index) => (
+          <div key={image.id + "-" + index} className="flex w-48 flex-col" >
             <img src={image.url} />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
