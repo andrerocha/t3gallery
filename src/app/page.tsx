bf412dddcebf7317186740b046ece0cdc5657899
André Rocha Araujo <andre_rocha@msn.com>
2024-11-04 23:25:49 -0300

    use auth for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
@@ -20,22 +21,33 @@ const mockImages = mockUrls.map((url, index) => ({
   url
 }));
 
-export default async function HomePage() {
-
+async function Images(){
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id)
   });  
 
+  return (      
+  <div className="flex flex-wrap gap-4">
+    {[...images, ...images, ...images].map((image, index) => (
+      <div key={image.id + "-" + index} className="flex w-48 flex-col" >
+        <img src={image.url} />
+        <div>{image.name}</div>
+      </div>
+    ))}
+  </div>)
+}
+
+export default async function HomePage() {
+
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((image, index) => (
-          <div key={image.id + "-" + index} className="flex w-48 flex-col" >
-            <img src={image.url} />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+
+      <SignedOut>
+        <div className="w-full h-hull text-2x1 text-center">Please sign in above</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
