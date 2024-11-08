af8d8168e8ac848943485fd2276f8893b6dd6dcc
André Rocha Araujo <andre_rocha@msn.com>
2024-11-08 00:29:52 -0300

    middleware adjust

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,12 @@ export default clerkMiddleware()
 
 export const config = {
   matcher: [
+    
+    "/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"
+
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    //'/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    //'/(api|trpc)(.*)',
   ],
 }
